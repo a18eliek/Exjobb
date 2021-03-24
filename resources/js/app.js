759f7e316cbb2dc5e62ac0fa58c32894d19d13ba
a18eliek <a18eliek@student.his.se>
2021-03-24 19:54:41 +0100

    Helper functions
    Added functions to start and end timers and print the difference

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,37 @@
+
+helpers = {
+    timerStart: function(func, file) {
+        var tpl = 'background-color:greenyellow; border:3px solid green; font-weight: bold;padding:3px 5px;color:';
+        console.log('%c[' + helpers.timeStamp() + ']' + '%c[start]' + '%c' + file + '::' + func, 
+                     tpl+'magenta', tpl+'red', tpl+'blue');
+        window['timer_' + file + '_' + func] = new Date();
+
+    },
+    timerEnd: function(func, file) {
+        var curTime = new Date();
+        var tpl = 'background-color:greenyellow; border:3px solid red; font-weight: bold;padding:3px 5px;color:';
+        console.log('%c[' + helpers.timeStamp() + ']' + '%c[end]' + '' + '%cTime: ' + (curTime - window['timer_' + file + '_' + func]) + ' ms' + '%c' + file + '::' + func, 
+                     tpl+'magenta', tpl+'red', tpl+'red', tpl+'blue');
+    },
+    timeStamp: function() {
+        let d = new Date();
+        // current hours
+        let hours = d.getHours();
+
+        // current minutes
+        let minutes = d.getMinutes();
+
+        // current seconds
+        let seconds = d.getSeconds();
+
+        // current milliseconds
+        let milliseconds = d.getMilliseconds();
+
+        // Return HH:mm:ss.S
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    }
+}
+
 require('./bootstrap');
 require('alpinejs');
 require('./react');
