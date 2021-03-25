603656a3e6e1227731bbc4c34410537a18f4803e
a18eliek <a18eliek@student.his.se>
2021-03-25 12:43:55 +0100

    Country specific data
    Weekly data for a specific country

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,60 @@ Route::get('/data', function () {
       exit;
 });
 
+// TODO: This needs to be adapted to work with any country
+Route::get('/data/swe', function () {
+    \Debugbar::disable();
+   
+    // $response = Http::get('https://opendata.ecdc.europa.eu/covid19/casedistribution/json');
+    $data = json_decode(file_get_contents('../storage/app/covid_data.json'));
+    $newData = [];
+    $counties = [];
+    $i = 0;
+    foreach($data as $key => $x) {
+        // List of all counties
+        if(isset($x->country_code)) { 
+            $counties['counties'][$x->country_code] = $x->country;
+        }
+        // Data for the specific country
+        if(isset($x->country_code) && $x->country_code == "SWE") {
+            $isoCodes = new \Sokil\IsoCodes\IsoCodesFactory();
+            $country = $isoCodes->getCountries()->getByAlpha3($x->country_code);
+            
+            // Some info regarding the country
+            if($i == 0) {
+                $newData = [
+                    "country" => $x->country,
+                    "country_alpha2" => $country->getAlpha2(),
+                    "country_code" => $x->country_code,
+                    "continent" => $x->continent,
+                    "population" => $x->population,
+                    "indicator" => $x->indicator,
+                    "source" => $x->source,
+                ];
+            }
+            $i++;
+
+            $date = explode("-",$x->year_week);
+            // The actual data regarding the country
+            $newData['data'][$x->year_week] = [
+                "weekly" => $x->weekly_count,
+                "cumulative" => $x->cumulative_count,
+                "rate_14_day" => isset($x->rate_14_day) ? (float)$x->rate_14_day : 0,
+                "start" => date( "Y-m-d", strtotime($date[0]."W".$date[1]."1") ),
+                "end" => date( "Y-m-d", strtotime($date[0]."W".$date[1]."7") ),
+                "indicator" => $x->indicator,
+            ];
+        }     
+    }
+
+    // Update the indicator, ECDC have a different one for the first ones it appears
+    $newData['indicator'] = end($newData['data'])['indicator'];
+
+    header('Content-Type: application/json');
+    echo json_encode($newData+$counties);
+    exit;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
