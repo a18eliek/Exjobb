375db4c84c12bc8131498c688ccc8208e9c38934
a18eliek <a18eliek@student.his.se>
2021-04-02 19:51:23 +0200

    React BarChart with the new API

--- resources/js/react/chart.tsx
+++ resources/js/react/chart.tsx
@@ -1,21 +1,66 @@
+import { Chart } from "react-google-charts";
 import * as React from "react";
 import { render } from "react-dom";
-import { Chart } from "react-google-charts";
+import Component from "@reach/component-component";
  
-export default class ReactChart extends React.Component {
-  render() {
-    return (
-        <Chart
-        chartType="ColumnChart"
-        data={[
-                ['Year', '1', '2', '3'],
-                ['2019', 1000, 400, 200],
-                ['2020', 1170, 460, 250],
-                ['2021', 660, 1120, 300]
-            ]}
-        legendToggle
-        />
-    );
-  }
-}
-render(<ReactChart />, document.querySelector("#react-chart"));
+
+const BarChart = () => {
+	return (
+		<Component 
+			initialState={{dataLoadingStatus: "loading", chartData:[]}}
+			didMount = {
+				async function(component) {
+					helpers.timerStart("didMount", "chart.tsx" );
+					$.ajax({
+						type: 'get',
+						url: '/data/',
+						dataType:"json",
+						success: function(response, status, jqXHR) {
+							helpers.timerStart("didMount->success", "chart.tsx" );
+
+							const chartColumns = [[
+								{ type: "string", id: "Country" },
+								{ type: "number", id: "Total Cases" },
+								{ type: "string", role: "tooltip" },
+								{ type: "number", id: "Total Deaths" },
+								{ type: "string", role: "tooltip" }
+							]];
+
+							const dataPoints = Object.entries(response).map(key => {
+								var tooltip = key[1].country + "\nTotal Cases: " + key[1].totalCases + "\nTotal Deaths: " + key[1].totalDeaths;
+								return [key[1].country, key[1].totalCases, tooltip, key[1].totalDeaths, tooltip];
+							});
+							
+							component.setState({
+								dataLoadingStatus: "ready",
+								chartData: chartColumns.concat(dataPoints)
+							});
+
+							helpers.timerEnd("didMount->success", "chart.tsx" );
+						}
+					});
+					helpers.timerEnd("didMount", "chart.tsx" );
+				}
+			}
+		>
+			{
+				(component) => {
+					return component.state.dataLoadingStatus === "ready" ? 
+					<Chart 
+						chartType="ColumnChart"
+						data={ component.state.chartData }
+						mapsApiKey={{ key: process.env.MIX_GOOGLE_MAPS_API }}
+						options={{
+            				isStacked: 'true',
+              				legend: { position: 'top', alignment: 'start' },
+							tooltip: { isHtml: false }
+						}}
+					/>
+					: <i className="fas fa-spinner fa-spin"></i>
+				}
+			}
+		</Component>
+  );
+  
+};
+render(<BarChart />, document.querySelector("#react-chart"));
