41dd128ec164ae003a4facd05d2897381abb7d4b
a18eliek <a18eliek@student.his.se>
2021-04-06 09:07:01 +0200

    Vue components will now only load if element exists

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -3,25 +3,36 @@ import vSelect from 'vue-select'
 import VueGoogleCharts from 'vue-google-charts'
 import Chart from './vue/chart.vue'
 import GeoChart from './vue/geochart.vue'
-// import Timeline from './vue/timeline.vue'
+import Timeline from './vue/timeline.vue'
 
 Vue.use(VueGoogleCharts)
 Vue.component('v-select', vSelect)
 
-const chart = new Vue({
-    el: '#vuejs-chart',
-    components: { Chart },
-    template: "<Chart/>"
-});
+window.onload = function () {
+    var chart = document.getElementById('vuejs-chart');
+    if (chart) {
+        const chart = new Vue({
+            el: '#vuejs-chart',
+            components: { Chart },
+            template: "<Chart/>"
+        });
+    }
 
-const geochart = new Vue({
-    el: '#vuejs-geochart',
-    components: { GeoChart },
-    template: "<GeoChart/>"
-});
+    var geochart = document.getElementById('vuejs-geochart');
+    if (geochart) {
+        const geochart = new Vue({
+            el: '#vuejs-geochart',
+            components: { GeoChart },
+            template: "<GeoChart/>"
+        });
+    }
 
-// const timeline = new Vue({
-//     el: '#vuejs-timeline',
-//     components: { Timeline },
-//     template: "<Timeline/>"
-// });
+    var timeline = document.getElementById('vuejs-timeline');
+    if (timeline) {
+        const timeline = new Vue({
+            el: '#vuejs-timeline',
+            components: { Timeline },
+            template: "<Timeline/>"
+        });
+    }
+};
