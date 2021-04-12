bb5f921f211c81d2ed1c6e898c87b190a8c09ee6
a18eliek <a18eliek@student.his.se>
2021-04-12 16:23:51 +0200

    Added ECDC info to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,8 @@
 <div class="justify-center mt-4 sm:items-center sm:justify-between">
     <div class="ml-4 text-center text-sm text-gray-500 sm:ml-0">
+        COVID-19 datasets provided by <a href="https://www.ecdc.europa.eu/en/covid-19/data" target="_blank">European Centre for Disease Prevention and Control (ECDC)</a>
+    </div>
+    <div class="ml-4 text-center text-sm text-gray-500 sm:ml-0">
         a18eliek - Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
     </div>
 </div>
